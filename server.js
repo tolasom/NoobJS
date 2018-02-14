59479fc2e63d0ab00ab43570a7c1a7990d81cb10
tolasom <somtola16@kit.edu.kh>
2018-02-14 12:29:39 +0700

    Initial COmmit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+hbs.registerHelper('help', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome too my website',
+        helpMe: 'what can i help you'
+    });
+
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'my About Page'
+            // currentYear: new Date().getFullYear()
+    });
+
+});
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        // currentYear: new Date().getFullYear(),
+        welcomeMessage: 'my Home Page'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help.hbs', {
+        pageTitle: 'Help ME',
+        helpMe: 'what can i help you'
+
+    })
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'unnable to reach the host'
+    });
+});
+
+app.listen(8000, () => {
+    console.log('server is running on port 8000');
+});
